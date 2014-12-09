e574c2abf371a20ec998fc7c0774a3c1e84606b0
Federico Binda <federico.binda@physics.uu.se>
2014-12-09 14:16:08 +0100

    dacsim.py added. General description of the code

---
+++ src/dacsim.py
@@ -0,0 +1,19 @@
+***
+dacsim
+======
+
+Data Acquisition Chain Simulation
+---------------------------------
+
+Simulate the data acquisition chain of a scintillator:
+
+ - scintillation process
+ - photomultiplier tube
+ - cable
+ - digitizer
+***
+
+import numpy as np
+import pylab as pl
+
+if __name__ == '__main__':
