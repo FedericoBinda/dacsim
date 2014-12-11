14c9e7bc3043e68bdf416f018c1d9cdbe8d60e68
Federico Binda <federico.binda@physics.uu.se>
2014-12-11 09:54:25 +0100

    First implementation of cable. Clearly something is wrong

--- src/cable.py
+++ src/cable.py
@@ -1,5 +1,5 @@
 import numpy as np
-from scipy import signal
+import os
 
 def get_cable_database():
     
@@ -16,11 +16,23 @@ def get_cable_database():
     cable_dict = {}
 
 
+    # temporary. implement proper database
+    cable_dict['RG58'] = np.array([[1,5,15,20,25,50],[0.05,0.2,1.0,3.3,8.0,30.0]]) 
 
-    cable_dict['RG58'] = [50] # temporary. implement proper database
     return cable_dict
 
 cable_dict = get_cable_database()
 
-def apply_cable(signal, t, cable_length = 10, cable_type = 'RG58'):
-    
+def apply_cable(signal, t, cable_length = 1, cable_type = 'RG58'):
+    try:
+        mask = (cable_dict[cable_type][0] == cable_length)
+    except KeyError:
+        print 'ERROR! cable type not present in database!'
+        return 0
+    if True not in mask: # NOTE: implement interpolation of length
+        print 'ERROR! cable length not present in database!'
+        return 0
+    print cable_dict[cable_type][1][mask]
+    tau = cable_dict[cable_type][1][mask][0]
+    cable_effect = np.exp(-t/tau)
+    return np.convolve(cable_effect, signal, 'same')
--- src/dacsim.py
+++ src/dacsim.py
@@ -17,17 +17,21 @@ Simulate the data acquisition chain of a scintillator:
 import pylab as pl
 import scintillator as sci
 import pmt
+import cable
 
 if __name__ == '__main__':
     t, amp_p = sci.scintillator('proton')
     t, amp_e = sci.scintillator('electron')
-    mypulses = sci.generate_pulses(1000,t,amp_p,10000)
+    mypulses = sci.generate_pulses(10,t,amp_p,10000)
     phots = mypulses[0]
-    pl.figure(1)
+    pl.figure(1) # scintillator
     hist, bins, patches = pl.hist(phots,bins=t,range=(min(t),max(t)))
+    pl.figure(2) # pmt
     newhist=pmt.apply_pmt(mypulses[0],t)
     pl.plot(t[:-1],newhist)
-    pl.figure(2)
-    pl.plot(t, amp_p)
-    pl.plot(t, amp_e)
+    pl.figure(3) # cable
+    new_y = cable.apply_cable(newhist, t[:-1])
+    pl.plot(t[:-1],new_y)
+    #pl.plot(t, amp_p)
+    #pl.plot(t, amp_e)
     pl.show()
